2086b147e97c814c23a4f71590ff15b3a2fd767b
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-10-24 11:44:36 -0700

    wave 3 completed

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,18 @@
-from flask import Flask
-from app.routes.Planets_routes import Planets_bp
 
-def create_app(test_config=None):
+
+from flask import Flask
+from .db import db, migrate
+from .routes.Planets_routes import Planets_bp
+
+def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(Planets_bp)
+
     return app
--- app/routes/Planets_routes.py
+++ app/routes/Planets_routes.py
@@ -1,42 +1,83 @@
-from flask import Blueprint , abort , make_response
-from app.models.Planets import planets
+from flask import Blueprint , abort , make_response , request
+from app.models.Planets import Planets 
+from ..db import db
 
 Planets_bp = Blueprint("Planets_bp", __name__, url_prefix="/planets")
 
-def validate_planet(id):
-    try:
-        id = int(id)
-    except:
-        response = {"message": f"planet {id} invalid"}
-        abort(make_response(response, 400))
 
-    for planet in planets:
-        if planet.id == id:
-            return planet
+@Planets_bp.post("")
+def create_planet():
+    request_body = request.get_json()
+    name = request_body["name"]
+    description = request_body["description"]
+    atmosphere = request_body["atmosphere"]
 
-    response = {"message": f"planet {id} not found"}
-    abort(make_response(response, 404))
+    new_planet = Planets(name=name, description=description, atmosphere = atmosphere)
+    db.session.add(new_planet)
+    db.session.commit()
+
+    response = {
+        "id": new_planet.id,
+        "name": new_planet.name,
+        "description": new_planet.description,
+        "atmosphere" : new_planet.atmosphere
+    }
+    return response, 201
+
+
 
 @Planets_bp.get("")
-def get_all_Planets():
-    
-    Planet_response = []
-    for Planet in planets:
-        Planet_response.append({
-            "id" : Planet.id , 
-            "name":Planet.name,
-            "description" : Planet.description , 
-            "atmosphere":Planet.atmosphere
-        })
+def get_all_planets():
+    query = db.select(Planets).order_by(Planets.id)
+    planets = db.session.scalars(query)
 
-    return Planet_response
-@Planets_bp.get("/<id>")
-def get_one_Planet(id):
-    planet = validate_planet(id)
-    return {
-            "id" : planet.id , 
-            "name":planet.name,
-            "description" : planet.description , 
-            "atmosphere":planet.atmosphere
-            }
+    Planets_response = []
+    for Planet in planets:
+        Planets_response.append(
+                {
+                    "id": Planet.id,
+                    "name": Planet.name,
+                    "description": Planet.description,
+                    "atmosphere": Planet.atmosphere
+                }
+            )
+    return Planets_response
+
+
+# # def validate_planet(id):
+# #     try:
+# #         id = int(id)
+# #     except:
+# #         response = {"message": f"planet {id} invalid"}
+# #         abort(make_response(response, 400))
+
+# #     for planet in planets:
+# #         if planet.id == id:
+# #             return planet
+
+# #     response = {"message": f"planet {id} not found"}
+# #     abort(make_response(response, 404))
+
+# # @Planets_bp.get("")
+# # def get_all_Planets():
+    
+# #     Planet_response = []
+# #     for Planet in planets:
+# #         Planet_response.append({
+# #             "id" : Planet.id , 
+# #             "name":Planet.name,
+# #             "description" : Planet.description , 
+# #             "atmosphere":Planet.atmosphere
+# #         })
+
+# #     return Planet_response
+# # @Planets_bp.get("/<id>")
+# # def get_one_Planet(id):
+# #     planet = validate_planet(id)
+# #     return {
+# #             "id" : planet.id , 
+# #             "name":planet.name,
+# #             "description" : planet.description , 
+# #             "atmosphere":planet.atmosphere
+# #             }
 
